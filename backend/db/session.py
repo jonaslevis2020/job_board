5d03b64a7553cc4e0dc3bd73cd0cbdc8fbe14ba5
Jonas Levis <41313961+jonaslevis2020@users.noreply.github.com>
2022-10-16 04:10:56 +0100

    create schemas

--- backend/db/session.py
+++ backend/db/session.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-
+from typing import Generator
 from core.config import settings
 
 
@@ -10,3 +10,10 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # Base = declarative_base()
+
+def get_db() -> Generator:
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
